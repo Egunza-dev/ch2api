f86efe06040ede68495cab4a7087393a90a2b78e
Kevin Egunza <egunza.dev@gmail.com>
2019-02-11 01:04:46 +0300

    [Finishes #163690142] Add GET /parties endpoint.

--- app/api/v1/parties/views.py
+++ app/api/v1/parties/views.py
@@ -1,3 +1,13 @@
 from flask import  request, jsonify, make_response 
 from app.api.v1 import api
 from app.api.v1.parties.models import Party
+
+
+@api.route('/parties', methods=['GET'], strict_slashes=False)
+def get_parties():
+    """Fetch all political parties records."""
+    parties = Party.get_parties()
+    return make_response(jsonify({
+        "status":200,
+        "data":parties
+    }), 200)
