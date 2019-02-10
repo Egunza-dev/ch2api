58025cad5047923dd1f4a76565fab5d01058de98
Kevin Egunza <egunza.dev@gmail.com>
2019-02-10 16:36:23 +0300

    [Finishes #163690252] Add GET /offices endpoint

--- app/api/v1/offices/views.py
+++ app/api/v1/offices/views.py
@@ -3,3 +3,11 @@ from app.api.v1 import api
 from app.api.v1.offices.models import Office
 
 
+@api.route('/offices', methods=['GET'], strict_slashes=False)
+def get_offices():
+    """Fetch all political offices records."""
+    offices = Office.get_offices()
+    return make_response(jsonify({
+        "status":200,
+        "data":offices
+    }), 200)
--- app/tests/test_offices.py
+++ app/tests/test_offices.py
@@ -1,8 +1,6 @@
 import unittest
 import os
 import sys
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
 import json
 from app import create_app
 
