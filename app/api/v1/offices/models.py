2af30d6af3ffa76db55718c6a61fcf7caec60948
Kevin Egunza <egunza.dev@gmail.com>
2019-02-10 15:46:25 +0300

    [Starts #163690252] Add failing test for GET /offices endpoint

--- app/api/v1/offices/models.py
+++ app/api/v1/offices/models.py
@@ -22,3 +22,6 @@ class Office:
             "name" : "Governor"
         }
     ]
+
+
+    
---
+++ app/tests/test_offices.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+import json
+from app import create_app
+
+
+class TestOfficesEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.office = {"type":"State",
+                        "name":"Secretary of State"}
+        self.office_err = {"type":"",
+                        "name":"Secretary of State"}
+
+    
+    def test_api_can_get_all_offices(self):
+        """Test endpoint that fetches all offices"""
+
+        res = self.client().get(path='/api/v1/offices/', content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('President', str(res.data))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
