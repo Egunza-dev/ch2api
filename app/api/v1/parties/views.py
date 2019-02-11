095be0ae25692599f313074e0383326532caf849
Kevin Egunza <egunza.dev@gmail.com>
2019-02-11 09:04:37 +0300

    [Finishes #163789398] Add GET /parties/<party-id>/ endpoint.

--- app/api/v1/parties/views.py
+++ app/api/v1/parties/views.py
@@ -11,3 +11,18 @@ def get_parties():
         "status":200,
         "data":parties
     }), 200)
+
+
+@api.route('/parties/<int:party_id>', methods=['GET'], strict_slashes=False)
+def get_party(party_id):
+    """Fetch a specific political party record."""
+    party = Party.get_party(party_id)
+    if party == None:
+        return make_response(jsonify({
+        "status":404,
+        "error":"The party does not exist on the Politico platform."
+    }), 404)
+    return make_response(jsonify({
+        "status":200,
+        "data":party
+    }), 200)
