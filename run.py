31e3aa2d7bbe56b807e95eca080f4924ce0bd27c
Kevin Egunza <egunza.dev@gmail.com>
2019-02-15 17:21:23 +0300

    Fix test coverage bugs

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from app import create_app
 config_name = os.getenv("APP_SETTINGS")
 
 app = create_app(config_name)
+from app.api import api as api_v1_blueprint
+app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
 
 if __name__ == '__main__':
     app.run()
